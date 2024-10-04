77c87cd45831c222f6d0807d539be1f79ffda95f
sheng232 <lowellchessofficial@gmail.com>
2024-10-03 19:44:29 -0700

    5th commit

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const startButton = document.querySelector(".fa-play");
 const pauseButton = document.querySelector(".fa-pause");
 const nextButton = document.querySelector(".fa-forward");
 const prevButton = document.querySelector(".fa-backward");
+const shuffleButton = document.querySelector(".fa-shuffle");
 const songTitle = document.querySelector(".title");
 const songsHTML = document.querySelector(".songs");
 
@@ -24,6 +25,22 @@ const mp3 = [
         artist: "some bird",
         albumImage: "image folder/Bird image.jpg",
         duration: "0:17"
+    },
+    {
+        id: 3,
+        title: "Goat sound",
+        src: "mp3 folder/Goat sound.mp3",
+        artist: "some goat",
+        albumImage: "image folder/Goat image.jpg",
+        duration: "0:05"
+    },
+    {
+        id: 4,
+        title: "Water sound",
+        src: "mp3 folder/Water sound.mp3",
+        artist: "water",
+        albumImage: "image folder/Water image.jpg",
+        duration: "0:11"
     }
 ];
 /*renders the song, pass the mp3 array and maps individual object into a new 
@@ -31,9 +48,9 @@ html snippet*/
 function renderSong(array){
     const HTML = array.map((song)=> {
       return `
-      <div class="song" id="Song${song.id}">
+      <div class="song">
       <button class="song-button grid center" onclick = "pressPlayAudio(${song.id})">
-        <div class="song-number">${song.id}</div>
+        <div class="song-number">${array.indexOf(song)+1}</div>
             <div class="flexbox">
             <img class="album" src="${song.albumImage}">
             <div class="song-title">${song.title}</div>
@@ -41,6 +58,7 @@ function renderSong(array){
             <div class="song-artist">${song.artist}</div>
             <div class="song-length">${song.duration}</div>
             </button>
+            <button class="button" onclick = "deleteSong(${song.id})"><i class="fa-solid fa-trash"></i></button>
         </div>
       `;
     })
@@ -51,6 +69,7 @@ renderSong(mp3);
 const currentSong = document.querySelectorAll(".song");
 let currentSongIndex = 0;
 let audio = new Audio(mp3[currentSongIndex].src);
+let isShuffle = false;
 songTitle.innerText = mp3[0].title;
 
 //Buttons
@@ -58,6 +77,7 @@ songTitle.innerText = mp3[0].title;
     pauseButton.addEventListener("click", pauseAudio);
     nextButton.addEventListener("click", nextAudio);
     prevButton.addEventListener("click", prevAudio);
+    shuffleButton.addEventListener("click", shuffle);
 //Ends the song
 
 //functions
@@ -68,6 +88,10 @@ function pressPlayAudio(id){
     playAudio();
 }
 function playAudio(){
+    if(isShuffle){
+        //implement shuffle feature
+    }
+    else{
     audio.play();
     //visual aspect of the code
     albumCover.style.animationPlayState = "running";
@@ -80,6 +104,7 @@ function playAudio(){
     currentSong[currentSongIndex].style.animation = "none";
     currentSong[currentSongIndex].offsetWidth; 
     currentSong[currentSongIndex].style.animation = "opacitate 1s";
+    }
 }
 function pauseAudio(){
     audio.pause();
@@ -109,4 +134,19 @@ function prevAudio(){
     audio = new Audio(mp3[currentSongIndex].src);
     playAudio();
     }
+}
+function shuffle(){
+    if(!isShuffle){
+        isShuffle = true;
+        shuffleButton.style.color = "yellow";
+    }
+    else{
+        isShuffle = false;
+        shuffleButton.style.color = "white";
+    }
+}
+function deleteSong(id){
+    currentSongIndex = id - 1;
+    mp3.splice(currentSongIndex, 1);
+    renderSong(mp3);
 }
